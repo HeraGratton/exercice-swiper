70729d03191c000ec9407af729bf57d64599f8d3
Hera Gratton <sonicrock4ever@gmail.com>
2020-11-24 10:34:57 -0500

    Exercices complétés

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.swiper-1', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -7,15 +7,78 @@ var mySwiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
     },
+
+  })
+
+  var mySwiper = new Swiper('.swiper-2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
+  })
+
+  var mySwiper = new Swiper('.swiper-3', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    disableOnInteraction: true,
+    autoplay: {
+        delay: 2000,
+      },
+  })
+
+  var mySwiper = new Swiper('.swiper-4', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+      },
+  })
+
+  var mySwiper = new Swiper('.swiper-5', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
+
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 780px
+        780: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 1000px
+        1000: {
+          slidesPerView: 4,
+          spaceBetween: 30
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 30
+        },
+      }
+  })
+
+  var mySwiper = new Swiper('.swiper-6', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: "flip",
   })
